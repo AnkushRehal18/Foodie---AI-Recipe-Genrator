3a6bce55beb3e81b367810767bea192708358d69
ankush <rehalankush192@gmail.com>
2026-07-04 20:00:40 +0530

    added the clerk biliing feature on the app

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,14 +1,17 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { PricingTable, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Cookie, Refrigerator } from 'lucide-react'
+import { Cookie, Refrigerator, Sparkle, Sparkles } from 'lucide-react'
 import UserDropdown from './UserDropdown'
+import { checkUser } from '@/lib/checkUser'
+import PricingModal from './PricingModal'
+import { Badge } from './ui/badge'
 
 const Header = async () => {
 
-    const user = null;
+    const user = await checkUser();
 
     return (
         <header className="fixed top-0 w-full  border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
@@ -40,7 +43,27 @@ const Header = async () => {
                 <div className='flex items-center space-x-4'>
                     <Show when="signed-in">
                         {/* How to Cook */}
-                       <UserDropdown/>
+
+                        {user && <PricingModal
+                            subscriptionTier={user.subscriptionTier}>
+                            <Badge variant='outline'
+                                className={`flex px-3 gap-1.5 rounded-full text-xs font-semibold transition-all
+                                ${user.subscriptionTier === "pro"
+                                        ? "bg-linear-to-r from-orange-600 to-amber-500 text-white border-none shadow-sm"
+                                        : "bg-stone-200/500 text-stone-600 border-stone-200 cursor-pointer hover:bg-stone-300/500 hover:border-stone-300"
+                                    }`}
+                            >
+                                <Sparkles
+                                    className={`h-3 w-3 ${user.subscriptionTier === "pro"
+                                            ? "text-white fiimport { PricingTable } from '@clerk/nextjs'll-white/20"
+                                            : "text-stone-500"
+                                        }`} />
+                                <span>
+                                    {user.subscriptionTier === "pro" ? "Pro Chef" : "Free Plan"}
+                                </span>
+                            </Badge>
+                        </PricingModal>}
+                        <UserDropdown />
                     </Show>
                     <Show when="signed-out">
                         <SignInButton mode='modal'>
