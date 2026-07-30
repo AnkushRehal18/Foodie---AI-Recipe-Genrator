6e9e90a64e29b2e60e0c8836838ed79a3cf16070
ankush <rehalankush192@gmail.com>
2026-07-30 22:55:06 +0530

    added server action for pantry section

---
+++ frontend/lib/arcjet.js
@@ -0,0 +1,44 @@
+import arcjet, { tokenBucket } from "@arcjet/next";
+
+export const aj = arcjet({
+    key: process.env.NEXT_PUBLIC_ARCJET_KEY,
+    rules:[]
+
+})
+
+//free tier pantry scans (limit it to 10 per month)
+
+export const freePantryScans = aj.withRule(
+    tokenBucket({
+        mode: "LIVE",
+        characteristics: ["userId"],
+        refillRate: 10,
+        interval: "30d",
+        capacity: 10
+    })
+)
+
+//free tier meal reccomendations (5 permonth)
+
+export const freeMealRecommendations = aj.withRule(
+    tokenBucket({
+        mode: "LIVE",
+        characteristics: ["userId"],
+        refillRate: 5,
+        interval: "30d",
+        capacity: 5,
+    })
+);
+
+//pro tier very high(will say 500)
+
+export const proTierLimit = aj.withRule(
+    tokenBucket({
+        mode: "LIVE",
+        characteristics: ["userId"],
+        refillRate: 500,
+        interval: "1d",
+        capacity: 500,
+    })
+);
+
